ae3bae2f570090bf0af144547e5d644fc2412975
Lausius <skinkens_email@yahoo.dk>
2020-11-05 12:47:20 +0100

    sut

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="main-app" class="container">
     <div class="row">
-      <SearchBar @search="searchRecords" />
-      <RecordCard :records="searchedRecords" />
+      <SearchBar
+        :myKey="filterKey"
+        :myDir="filterDir"
+        @search="searchRecords"
+        @request-key="changeKey"
+        @request-dir="changeDir"
+      />
+      <RecordCard :records="filteredRecords" />
     </div>
   </div>
 </template>
@@ -10,12 +16,15 @@
 <script>
 import RecordCard from "./components/RecordCard";
 import SearchBar from "./components/SearchBar";
+import _ from "lodash";
 
 export default {
   data: function () {
     return {
       records: [],
       searchTerms: "",
+      filterKey: "title",
+      filterDir: "asc"
     };
   },
   components: {
@@ -23,15 +32,15 @@ export default {
     SearchBar,
   },
   mounted() {
-        fetch("https://drmusicrestservice.azurewebsites.net/api/DRMusicRecord")
-      .then(response => response.json())
-      .then(data => {
+    fetch("https://drmusicrestservice.azurewebsites.net/api/DRMusicRecord")
+      .then((response) => response.json())
+      .then((data) => {
         this.records = data;
       });
   },
   computed: {
     searchedRecords: function () {
-      return this.records.filter(item => {
+      return this.records.filter((item) => {
         return (
           item.title.toLowerCase().match(this.searchTerms.toLowerCase()) ||
           item.artist.toLowerCase().match(this.searchTerms.toLowerCase()) ||
@@ -39,11 +48,26 @@ export default {
         );
       });
     },
+    filteredRecords: function() {
+      return _.orderBy(
+        this.searchedRecords, item => {
+          console.log(this.filterKey);
+          return item[this.filterKey].toLowerCase();
+        },
+        this.filterDir
+      );
+    }
   },
   methods: {
-    searchRecords: function(searchTerm) {
+    searchRecords: function (searchTerm) {
       this.searchTerms = searchTerm;
     },
+    changeKey: function (value) {
+      this.filterKey = value;
+    },
+    changeDir: function ( value) {
+      this.filterDir = value;
+    }
   },
 };
 </script>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,7 +8,74 @@
         class="form-control"
         aria-label="Search Appointments"
         v-model="searchTerm"
-      >
+      />
+
+      <div class="input-group-append">
+        <button
+          type="button"
+          class="btn btn-primary dropdown-toggle"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Sort by
+          <span class="caret"></span>
+        </button>
+
+        <div class="dropdown-menu dropdown-menu-right">
+          <a
+            href="#"
+            class="dropdown-item d-flex justify-content-between"
+            id="title"
+            @click="$emit('request-key', 'title')"
+          >
+            Title
+            <div>*</div>
+          </a>
+
+          <a
+            class="dropdown-item d-flex justify-content-between"
+            href="#"
+            id="artist"
+            @click="$emit('request-key', 'artist')"
+          >
+            Artist
+            <div>*</div>
+          </a>
+
+          <a
+            href="#"
+            class="dropdown-item d-flex justify-content-between"
+            id="album"
+            @click="$emit('request-key', 'album')"
+          >
+            Album
+            <div v-if="myKey == 'album'">*</div>
+          </a>
+
+          <div class="dropdown-divider" role="separator"></div>
+
+          <a
+            class="dropdown-item d-flex justify-content-between"
+            href="#"
+            id="asc"
+            @click="$emit('request-dir', 'asc')"
+          >
+            Asc
+            <div v-if="myDir === 'asc'">*</div>
+          </a>
+
+          <a
+            class="dropdown-item d-flex justify-content-between"
+            href="#"
+            id="desc"
+            @click="$emit('request-dir', 'desc')"
+          >
+            Desc
+            <div v-if="myDir === 'desc'">*</div>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,14 +85,14 @@ export default {
   name: "SearchBar",
   data() {
     return {
-      filters: "",
       searchTerm: "",
     };
   },
   watch: {
     searchTerm: function () {
       this.$emit("search", this.searchTerm);
-    }
+    },
+    props: ["myKey", "myDir"],
   },
 };
 </script>
